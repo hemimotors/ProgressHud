21085076f7857b80fff1d571b776e352b648b62f
dmitrijkotikov <kotikov.dmitrij@gmail.com>
2016-07-26 11:12:32 +0300

    init

---
+++ src/main/java/ua/privatbank/progresshud/ProgressHud.java
@@ -0,0 +1,21 @@
+package ua.privatbank.progresshud;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ProgressHud extends View {
+
+    public ProgressHud(Context context) {
+        super(context);
+    }
+
+    public ProgressHud(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ProgressHud(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
